81e54063f4c71f527cfcebf33a0f420e5f8328f2
Jugg <464664972@qq.com>
2016-04-13 18:43:49 +0800

    2016-04-13 18:43
    2016-04-13 18:43

--- ExpToNFA.cs
+++ ExpToNFA.cs
@@ -8,7 +8,6 @@ namespace DFA
 {
     public class ExpToNFA
     {
-
         //正规式转化为后缀表达式
         public string ExpToBack(string Exp)
         {
@@ -16,22 +15,31 @@ namespace DFA
         }
     }
 
-    public class ThompsonMinNode
+    public class NFANode
     {
-        public int Index;
-        public string Name;
-        public ThompsonMinNode(int index,string name)
+        int NodeID;
+        bool isEmpty;
+
+
+        public NFANode(int NodeID, bool isEmpty)
         {
-            this.Index = index;
-            this.Name = name;
+            this.NodeID = NodeID;
+            this.isEmpty = isEmpty;
         }
     }
+
+    public class ReplaceRecord
+    {
+        int BeReplaced;
+        int result;
+    }
+
     public class ThompsonNode
     {
-        public string from;
+        public NFANode from;
         public string edge;
-        public string to;
-        public ThompsonNode(string from, string edge, string to)
+        public NFANode to;
+        public ThompsonNode(NFANode from, string edge, NFANode to)
         {
             this.from = from;
             this.edge = edge;
@@ -39,56 +47,38 @@ namespace DFA
         }
     }
 
-    public class ThompsonNode_Digit
-    {
-        public int from;
-        public string edge;
-        public int to;
-        public ThompsonNode_Digit(int from, string edge, int to)
-        {
-            this.from = from;
-            this.edge = edge;
-            this.to = to;
-        }
-    }
 
     public class Sigle
     {
 
-        public ThompsonNode_Digit t;
-        public Sigle(int startIndex)
+        public Sigle(string edge, ref int startIndex, ref List<ThompsonNode> ThompsonNodeList,int Type)
         {
-            t = new ThompsonNode_Digit(startIndex, "ε", startIndex+1);
+            if(Type==1)
+            {
+                NFANode a = new NFANode(startIndex, false);
+                startIndex++;
+                NFANode b = new NFANode(startIndex, false);
+                startIndex++;
+                ThompsonNode aa = new ThompsonNode(a, edge, b);
+                ThompsonNodeList.Add(aa);
+            }
+            else
+            {
+
+            }
+
         }
+
     }
 
     public class Or
     {
-        public ThompsonNode_Digit t1;
-        public ThompsonNode_Digit t2;
-        public ThompsonNode_Digit t3;
-        public ThompsonNode_Digit t4;
         public Or(int startIndex)
         {
             t1 = new ThompsonNode_Digit(startIndex, "ε", startIndex + 1);
             t2 = new ThompsonNode_Digit(startIndex, "ε", startIndex + 3);
             t3 = new ThompsonNode_Digit(startIndex + 2, "ε", startIndex + 5);
             t4 = new ThompsonNode_Digit(startIndex + 4, "ε", startIndex + 5);
-        }
-        public List<ThompsonNode_Digit> LinkOr(List<ThompsonNode_Digit> ThompsonNode_DigitList)
-        {
-            
-            List<ThompsonNode_Digit> t = new List<ThompsonNode_Digit>();
-            t.Add(t1);
-            t.Add(t2);
-            t.Add(t3);
-            t.Add(t4);
-
-            for (int i = 0; i < ThompsonNode_DigitList.Count; i++)
-            {
-                t.Add(ThompsonNode_DigitList[i]);
-            }
-            return t;
         }
     }
 
